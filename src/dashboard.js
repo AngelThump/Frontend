10c85eda837f3c07fb199f4c554c27db154f8bce
TimIsOverpowered <admin@angelthump.com>
2020-05-12 02:52:22 +0000

    add links to channel for new users

--- src/dashboard.js
+++ src/dashboard.js
@@ -217,6 +217,8 @@ class Dashboard extends Component {
                             </div>
                           )}
                         </div>
+                        <h2 className="at-pd-t-2 at-pd-b-1" style={{backgroundColor: "#0e0e10", marginBottom: "0rem"}}>Channel Page: <a href={`/${user.username}`}>https://angelthump.com/{user.display_name}</a></h2>
+                        <h2 className="at-pd-t-2 at-pd-b-1" style={{backgroundColor: "#0e0e10", marginBottom: "0rem"}}>Player: <a href={`/${user.username}/embed`}>https://angelthump.com/{user.display_name}/embed</a></h2>
                       </div>
                     </div>
                   </div>
