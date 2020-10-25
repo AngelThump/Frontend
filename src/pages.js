01810bf8c091fa426bd0e37241f1c9f9de8cb7cb
TimIsOverpowered <admin@angelthump.com>
2020-10-25 18:28:44 -0500

    pages rework

--- src/pages.js
+++ src/pages.js
@@ -1,47 +1,24 @@
-import React, { Component } from 'react';
-import TermsOfService from './pages/tos';
-import PrivacyPolicy from './pages/privacypolicy';
-import DMCA from './pages/dmca';
-import NotFound from './notfound';
-import SimpleBar from 'simplebar-react';
-import './css/pages.css';
+import React from "react";
+import TermsOfService from "./pages/tos";
+import PrivacyPolicy from "./pages/privacypolicy";
+import DMCA from "./pages/dmca";
+import NotFound from "./notfound";
+import SimpleBar from "simplebar-react";
+import "./css/pages.css";
 
-class Pages extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    };
-  }
-
-  componentDidMount() {
-    
-  }
-
-  render() {
-    const page = this.props.match.params.pages.toLowerCase();
-    if(page === 'tos') {
-      return (
-        <SimpleBar style={{height: "100%"}}>
-          <TermsOfService/>
-        </SimpleBar>
-      )
-    } else if(page === 'privacy') {
-      return (
-        <SimpleBar style={{height: "100%"}}>
-          <PrivacyPolicy/>
-        </SimpleBar>
-      )
-    } else if (page === 'dmca') {
-      return (
-        <SimpleBar style={{height: "100%"}}>
-          <DMCA/>
-        </SimpleBar>
-      )
-    } else {
-      return <NotFound/>;
-    }
-  }
+export default function Pages(props) {
+  const page = props.match.params.pages.toLowerCase();
+  return (
+    <SimpleBar style={{ height: "100%" }}>
+      {page === "tos" ? (
+        <TermsOfService />
+      ) : page === "privacy" ? (
+        <PrivacyPolicy />
+      ) : page === "dmca" ? (
+        <DMCA />
+      ) : (
+        <NotFound />
+      )}
+    </SimpleBar>
+  );
 }
-
-export default Pages;
--- src/tracking.js
+++ src/tracking.js
@@ -3,26 +3,25 @@ import ReactGA from "react-ga";
 const trackingID = "UA-98637299-1";
 
 export const initGA = () => {
-    ReactGA.initialize(trackingID, {
-        debug: false
-    });
-}
+  ReactGA.initialize(trackingID, {
+    debug: false,
+  });
+};
 
 export const PageView = () => {
-    ReactGA.pageview(window.location.pathname +  
-                     window.location.search); 
-}
+  ReactGA.pageview(window.location.pathname + window.location.search);
+};
 
 /**
  * Event - Add custom tracking event.
- * @param {string} category 
- * @param {string} action 
- * @param {string} label 
+ * @param {string} category
+ * @param {string} action
+ * @param {string} label
  */
 export const Event = (category, action, label) => {
-    ReactGA.event({
-      category: category,
-      action: action,
-      label: label
-    });
+  ReactGA.event({
+    category: category,
+    action: action,
+    label: label,
+  });
 };
